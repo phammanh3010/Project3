eaed109c3206e00d0142a5b9b50781fbde57983e
phammanh3010 <31407878+phammanh3010@users.noreply.github.com>
2018-11-17 23:51:12 +0700

    Add files via upload

---
+++ quanlydoan/database/migrations/2018_11_12_023757_create_teachers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeachersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->integer('id_teacher')->unsigned();
+            $table->string('workplace');
+            $table->timestamps();
+            $table->foreign('id_teacher')->references('id_account')->on('accounts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teachers');
+    }
+}
