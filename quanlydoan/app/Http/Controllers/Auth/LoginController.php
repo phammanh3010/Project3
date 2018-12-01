77d8cd4db98b7230846106be9eadd1ab8f93637c
phammanh3010 <31407878+phammanh3010@users.noreply.github.com>
2018-12-01 10:06:12 -0500

    Add files via upload

--- quanlydoan/app/Http/Controllers/Auth/LoginController.php
+++ quanlydoan/app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Session;
 
 class LoginController extends Controller
 {
@@ -25,7 +29,19 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected function authenticated($request, $user)
+    {
+    if($user->position ==1){
+    return redirect('/student');
+    }elseif ($user->position ==2) {
+    return redirect('/teacher');
+    }elseif ($user->position ==3) {
+    return redirect('/admin');
+    }else {
+    return redirect('/');
+    }
+    }
+    # $redirectTo = '/home';
 
     /**
      * Create a new controller instance.
@@ -36,4 +52,33 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function username()
+    {
+    return 'username';
+    }
+    protected function credentials(Request $request)
+    {
+        $field = filter_var($request->get($this->username()), FILTER_VALIDATE_EMAIL)
+            ? $this->username()
+            : 'username';
+
+        return [
+            $field => $request->get($this->username()),
+            'password' => $request->password,
+        ];
+    }
+
+    protected $redirectAfterLogout = '/';
+
+    public function logout()
+    {
+        $user = Auth::user();
+        Log::info('User Logged Out. ', [$user]);
+        Auth::logout();
+        Session::flush();
+        return redirect(property_exists($this, 'redirectAfterLogout') ? $this->redirectAfterLogout : '/');
+    }
+
+
 }
