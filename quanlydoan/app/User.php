ab7fcb72cf874c0d4120a22b6ca3ee8d906cd2f7
phammanh3010 <31407878+phammanh3010@users.noreply.github.com>
2018-12-01 10:49:24 -0500

    Update User.php

--- quanlydoan/app/User.php
+++ quanlydoan/app/User.php
@@ -3,8 +3,10 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model
+class User extends Authenticatable
 {
     //
     protected $table = 'user';
