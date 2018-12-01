4bb52ff1037c6ee635cdeb7b6a0468ce45672383
phammanh3010 <31407878+phammanh3010@users.noreply.github.com>
2018-12-01 09:57:33 -0500

    Add files via upload

---
+++ quanlydoan/database/migrations/2018_11_18_014903_create_user_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->string('username', 45);
+            $table->primary('username');
+            $table->string('password', 100);
+            $table->integer('position');
+            $table->string('full_name', 100)->nullable();
+            $table->string('email', 100)->nullable();
+            $table->string('phone', 10)->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+}
