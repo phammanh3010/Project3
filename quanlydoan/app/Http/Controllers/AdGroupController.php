e9d6652cb8ae51201e6d5f96a290bab47c54ad13
phammanh3010 <31407878+phammanh3010@users.noreply.github.com>
2018-12-09 10:21:33 -0500

    Update AdGroupController.php

--- quanlydoan/app/Http/Controllers/AdGroupController.php
+++ quanlydoan/app/Http/Controllers/AdGroupController.php
@@ -40,7 +40,7 @@ class AdGroupController extends Controller{
 
         $group->save();
 
-        Storage::makeDirectory($request->groupname);
+        Storage::makeDirectory($request->semester."/".$request->subjectname."/".$group->id_group);
 
         return redirect(url('teacher/'))->with('thongbao','Bạn đã thêm thành công');
     }
